b4f2597e8532c3308335e8ca307a26b2adb755d4
Jo Byeong Wook <whquddnr97@gmail.com>
2022-05-27 14:49:54 +0900

    init

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "saki-chan.dev",
+  description: "dev blog",
+  author: "saki-chan",
+  siteUrl: "https://saki-chan.dev/",
+  links: {
+    github: "https://github.com/",
+    email: "admin@saki-chan.dev"
+  },
+  utterances: {
+    repo: "whquddnr97/dev-blog",
+    type: "pathname",
+  },
+}
---
+++ contents/posts/test/index.md
@@ -0,0 +1,9 @@
+---
+title: "test"
+description: "test"
+date: 2021-05-27
+update: 2021-05-27
+tags:
+  - test
+---
+### test
